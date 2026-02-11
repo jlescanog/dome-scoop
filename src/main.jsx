d37c8795b31f5d21284a7ee0d7bcf83bb639c58a
Jhair Lescano mint <jlescanoguevara@gmail.com>
2026-02-11 00:43:43 -0500

    agrego SEO open graph

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactGA from "react-ga4" // <--- 1. Importamos la librería
 import './index.css'
 import App from './App.jsx'
+
+// --- CONFIGURACIÓN DE ANALYTICS ---
+
+// 2. Inicializar (Reemplaza con tu ID real de Google Analytics)
+ReactGA.initialize("G-BE4GRTQRVG"); 
+
+// 3. Enviar la primera visita (Pageview)
+ReactGA.send({ 
+  hitType: "pageview", 
+  page: window.location.pathname,
+  title: "Visita Inicial"
+});
+
+// ----------------------------------
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
